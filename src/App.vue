48ba062c56fab9def49c77c775f8f991c8173536
alefagi <iltu@libero.it>
2021-09-15 14:14:45 +0200

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <Navbar />
+    </header>
+    <main></main>
+    <footer></footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+  },
+  data() {
+    return {
+      
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/scss/style.scss";
+
+
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,109 @@
+<template>
+  <nav>
+    <div class="container">
+      <div class="logo">
+        <img src="../assets/img/logo-dark.png" alt="logo-dark">
+      </div>
+      <ul class="menu">
+        <li v-for="(item, index) in navItems" :key="index">
+          <a :href="item.url">
+            {{ item.text }} <i class="fas fa-angle-down" v-if="item.text!='home'"></i>
+          </a>
+        </li>
+      </ul>
+      <div class="utils-icons">
+        <a href="#"><i class="fas fa-search"></i></a>
+        <a href="#"><i class="fab fa-github"></i></a>
+        <a href="#"><i class="fab fa-stack-overflow"></i></a>
+        <a href="#"><i class="far fa-user"></i></a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      navItems: [
+        {
+          text: 'home',
+          url: '#',
+        },
+        {
+          text: 'landing',
+          url: '#',
+        },
+        {
+          text: 'pages',
+          url: '#',
+        },
+        {
+          text: 'docs',
+          url: '#',
+        }
+      ],
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/scss/variables.scss";
+
+  .container {
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  nav {
+    height: 56px;
+    background-color: $brand-primary;
+  } 
+  .logo {
+    height: 20px;
+  } 
+  ul {
+    display: flex;
+    li {
+      list-style-type: none;
+      text-transform: uppercase;
+      font-size: 0.8rem;
+      font-weight: bold;
+      a {
+        text-decoration: none;
+        color: #3C4858;
+        padding: 20px 20px;
+        &:hover {
+          color: $brand-secondary;
+        }
+      }
+    }
+  }
+  .utils-icons a {
+    display: inline-block;
+    height: 35px;
+    width: 35px;
+    padding: 10px;
+    margin-left: 5px;
+    border-radius: 5px;
+    box-shadow: 1px 3px 10px rgba(0, 0, 0, .2);
+    color: $brand-secondary;
+    &:hover {
+      color: #fff;
+      background-color: $brand-secondary;
+    }
+    &:first-child {
+      color: #8492A6;
+      box-shadow: none;
+      &:hover {
+        color: #3C4858;
+        background-color: $brand-primary; 
+      }
+    }
+  }
+
+
+</style>
